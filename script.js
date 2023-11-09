f3432849dd64c7507cb9434257daedaf26eff472
duoganh <150222479+duoganh@users.noreply.github.com>
2023-11-09 21:05:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function onDevToolsOpen(){
      setTimeout(console.clear.bind(console))
           setTimeout(function() {
-                 window.location.href = 'https://www.youtube.com/watch?v=-D_oQrvXknM'; }); 
+                 window.location.href = 'https://duonganh.github.io.vn/good/'; }); 
                  const script = document.querySelector('script');
                  script.remove();
  
