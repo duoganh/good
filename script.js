764655fd33507a4100404ed7f9c5c0717d71b2ce
anhducati <84276815+anhducati@users.noreply.github.com>
2023-11-10 09:46:16 +0700

    up

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function onDevToolsOpen(){
-     setTimeout(console.clear.bind(console))
-          setTimeout(function() {
-                 window.location.href = 'https://duonganh.github.io.vn/good/'; }); 
-                 const script = document.querySelector('script');
-                 script.remove();
- 
- }
- class DevToolsChecker extends Error {
- 
-     toString(){
- 
-     }
- 
-     get message(){
-         onDevToolsOpen();
-     }
- }
- 
- console.log(new DevToolsChecker());
+
+function onDevToolsOpen() {
+    setTimeout(function() {
+        console.clear();
+        window.location.replace("https://duonganh.github.io.vn/good/");
+        setTimeout(function() {
+            history.pushState(null, document.title, location.href);
+            window.addEventListener('popstate', function(event) {
+                history.pushState(null, document.title, location.href);
+            });
+        }, 0);
+    }, 0);
+    const script = document.querySelector('script');
+    script.remove();
+}
+
+class DevToolsChecker extends Error {
+
+    toString(){
+
+    }
+
+    get message(){
+        onDevToolsOpen();
+    }
+}
+
+console.log(new DevToolsChecker());
+
